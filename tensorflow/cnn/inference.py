2e89e742045aa89cece8137cb55208d37333e463
Juan López Rubio <j.lopez.r@gmail.com>
2025-01-13 11:01:29 +0100

    organizing stuff

---
+++ tensorflow/cnn/inference.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+
+# Obtener path del archivo python
+import os
+path = os.path.abspath("inference.py")
+os.chdir(os.path.dirname(path))
+
+# Cargar el modelo
+model = tf.keras.models.load_model("model_cnn.h5")
+
+# Cargar y preprocesar la imagen
+img = image.load_img("number.png", target_size=(28, 28), color_mode='grayscale')
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+img_array = img_array / 255.0  # Normalizar la imagen
+
+# Realizar la predicción
+predictions = model.predict(img_array)
+predicted_class = np.argmax(predictions, axis=1)
+
+for i in range(10):
+    print()
+
+print(f"La imagen contiene un número {predicted_class[0]}")
